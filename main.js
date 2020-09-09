6332219f330ff03caa167a239a6fe3cf377650a3
Romie Hecdivert <65568062+rh9891@users.noreply.github.com>
2020-09-09 08:33:58 -0400

    Created function to generate random uppercase.

--- main.js
+++ main.js
@@ -2,3 +2,8 @@
 function getRandomLower() {
     return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 }
+
+// Function to generate random uppercase.
+function getRandomUpper() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
