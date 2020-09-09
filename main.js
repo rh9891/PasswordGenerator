5d9edf3bf72e45c3a10a1d83ad9ea962bdf4a1f1
Romie Hecdivert <65568062+rh9891@users.noreply.github.com>
2020-09-09 09:05:31 -0400

    Added an event listener to the generate button.

--- main.js
+++ main.js
@@ -8,31 +8,40 @@ const symbolsElement = document.getElementById("symbols");
 const generateElement = document.getElementById("generate");
 const clipboardElement = document.getElementById("clipboard");
 
-
 const randomFunction = {
     lower: getRandomLower,
     upper: getRandomUpper,
     number: getRandomNumber,
     symbol: getRandomSymbol
-}
+};
+
+generateElement.addEventListener("click", () => {
+    const length = +lengthElement.value;
+    const hasLower = lowercaseElement.checked;
+    const hasUpper = uppercaseElement.checked;
+    const hasNumber = numbersElement.checked;
+    const hasSymbol = symbolsElement.checked;
+
+    console.log(hasLower, hasNumber, hasSymbol, hasUpper);
+})
 
 // Function to generate random lowercase.
 function getRandomLower() {
     return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-}
+};
 
 // Function to generate random uppercase.
 function getRandomUpper() {
     return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-}
+};
 
 // Function to generate random numbers.
 function getRandomNumber() {
     return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
-}
+};
 
 // Function to generate random symbol.
 function getRandomSymbol() {
     const symbols = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~";
     return symbols[Math.floor(Math.random() * symbols.length)];
-}
+};
