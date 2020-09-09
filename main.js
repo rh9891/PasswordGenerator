0438718093ba3a7a54d4f9d0d49e5b84b1415e04
Romie Hecdivert <65568062+rh9891@users.noreply.github.com>
2020-09-09 08:37:08 -0400

    Created function to generate random number.

--- main.js
+++ main.js
@@ -7,3 +7,8 @@ function getRandomLower() {
 function getRandomUpper() {
     return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
 }
+
+// Function to generate random numbers.
+function getRandomNumber() {
+    return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
